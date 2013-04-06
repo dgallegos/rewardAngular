fe9d862565f51fbbfbecbfd76e553b1acc0e1d0d
Marshall <a530698.dmn1@macla990976.tx.rr.com>
2013-04-06 18:23:08 -0500

    updated server.js

---
+++ app/modules/logging.js
@@ -0,0 +1,10 @@
+    var bloggerTokens = [{
+            name: "Gabe Marshall",
+            token: "325c1cd5606244517254b720e21258c8"
+        }, {
+            name: "David Gallegos",
+            token: "b90afc2290d9e37d432fa6b4e4dea0d0"
+        }
+    ];
+
+    module.exports = bloggerTokens;
--- app/server.js
+++ app/server.js
@@ -1,8 +1,54 @@
-﻿var express = require('express');
+﻿var express = require('express')
+  , passport = require('passport')
+  , util = require('util')
+  , TwitterStrategy = require('passport-twitter').Strategy;
+
+var TWITTER_CONSUMER_KEY = "fTxFCJK61JT8YijMDlykjw";
+var TWITTER_CONSUMER_SECRET = "iqbk2wvvsw9iPifpia7mxQvjdM5Ty3Nad7nKJ3sRc8";
+
 var http = require('https');
 var Firebase = require('firebase');
 var async = require('async');
+var bloggerTokens = require('./modules/bloggertokens');
 
+
+
+/* Authentication */
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the TwitterStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a token, tokenSecret, and Twitter profile), and
+//   invoke a callback with a user object.
+passport.use(new TwitterStrategy({
+    consumerKey: TWITTER_CONSUMER_KEY,
+    consumerSecret: TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://rewardrobe.jit.su/authenticated"
+  },
+  function(token, tokenSecret, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Twitter profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Twitter account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+/* End of Authentication */
+
+console.log(typeof bloggerTokens);
 
 var options = {
   host: 'api.rewardstyle.com',
@@ -11,11 +57,34 @@ var options = {
 
 var app = express();
 
-app.configure(function () {
-    app.use('/', express.static(__dirname));
+// app.configure(function () {
+//     app.use('/', express.static(__dirname));
+// });
+
+
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.logger());
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/'));
 });
 
-app.get('/json', function (request, response) {
+app.get('/', ensureAuthenticated, function (request, response){
+    response.render('index.html');
+})
+
+
+
+app.get('/json', ensureAuthenticated, function (request, response) {
 
     // Grab request parameters to perform search
 
@@ -34,8 +103,6 @@ app.get('/json', function (request, response) {
         var data = ''
 
         res.on('data', function (chunk) {
-            // console.log('BODY: ' + chunk);
-            // response.json(chunk);
             data += chunk;
             console.log(chunk);
 
@@ -47,11 +114,6 @@ app.get('/json', function (request, response) {
         })
 
 
-
-        // }).on('error', function(e) {
-        //   console.log('ERROR: ' + e.message);
-        // });
-
     });
 
 });
@@ -61,20 +123,9 @@ app.get('/json', function (request, response) {
 
 app.get('/favorites', function (request, response) {
 
-    // Grab request parameters to perform search
+    // if (request.isAuthenticated())
 
-    // var searchToken = request.query ['token'];
 
-    var bloggerTokens = [{
-            name: "Gabe Marshall",
-            token: "325c1cd5606244517254b720e21258c8"
-        }, {
-            name: "David Gallegos",
-            token: "b90afc2290d9e37d432fa6b4e4dea0d0"
-        }
-    ];
-
-    // console.log(bloggerTokens[1]);
     var jsonArray = [];
 
     var returnFavorites = function (token, callback) {
@@ -125,6 +176,18 @@ app.get('/favorites', function (request, response) {
 
 });
 
+app.get('/auth/twitter',
+  passport.authenticate('twitter'),
+  function(req, res){
+    // The request will be redirected to Twitter for authentication, so this
+    // function will not be called.
+});
+
 
 
 app.listen(8000);
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login.html')
+}
