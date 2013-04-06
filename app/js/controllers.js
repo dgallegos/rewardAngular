5fb8483b5d9ce3a693cd5e49c9dc7f266fec01e2
unknown <GallegosFam@.(none)>
2013-04-06 13:46:58 -0500

    Add css and first json get

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,10 +7,27 @@
 function RewardCtrl($scope,RewardService)
 {
   $scope.hello = "Hello World";
+  $scope.publishers = [{name:"Gabe Marshall",token:"325c1cd5606244517254b720e21258c8"},
+                        {name:"David Gallegos",token:"b90afc2290d9e37d432fa6b4e4dea0d0"}];
+
+
+  $scope.getFavorites = function(token)
+  {
+      var url = 'https://localhost:8000/favorites?oauth_token='+token;
+
+      $.getJSON(url, function(json) {
+        console.log(json, json.name);
+        $scope.$apply(function(){
+            $scope.favorites = json;
+        });
+      });
+  }
+
 
   RewardService.getFavorites.async().then(function(returnJson) {
       // Assign return data to Client Object
       $scope.favorites = returnJson;
     });
+
 
 }
--- app/js/services.js
+++ app/js/services.js
@@ -13,7 +13,7 @@ angular.module('rewardStyleServices', ['ngResource']).
     RewardService.getFavorites = {
       async: function() {
         // $http returns a promise, which has a then function, which also returns a promise
-        var promise = $http.get('/favorites').then(function (response) {
+        var promise = $http.get('/json').then(function (response) {
           // The then function here is an opportunity to modify the response
           console.log(response);
           // The return value gets picked up by the then in the controller.
@@ -24,7 +24,11 @@ angular.module('rewardStyleServices', ['ngResource']).
       }
     };
 
+    RewardService.favorites = {
+      async: function() {
 
+      }
+    };
     return RewardService;
   });
 
