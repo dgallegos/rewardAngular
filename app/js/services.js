5e924b412ada5f4f4b9e2a2e4248611723e1a534
unknown <GallegosFam@.(none)>
2013-04-06 21:41:43 -0500

    remove console.log

--- app/js/services.js
+++ app/js/services.js
@@ -17,8 +17,6 @@ angular.module('rewardStyleServices', ['ngResource']).
 
         // $http returns a promise, which has a then function, which also returns a promise
         var promise = $http.get(favoritesRequest).then(function (response) {
-          // The then function here is an opportunity to modify the response
-          console.log(response);
           // The return value gets picked up by the then in the controller.
           return response.data;
         });
@@ -34,8 +32,6 @@ angular.module('rewardStyleServices', ['ngResource']).
 
         // $http returns a promise, which has a then function, which also returns a promise
         var promise = $http.get(favoritesRequest).then(function (response) {
-          // The then function here is an opportunity to modify the response
-          console.log(response);
           // The return value gets picked up by the then in the controller.
           return response.data;
         });
