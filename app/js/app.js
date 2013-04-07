92aa324492a35b47a5aacacadd449c808650508b
unknown <GallegosFam@.(none)>
2013-04-07 10:49:24 -0500

    User Angular routeprovider for templating.
    Add initial publishers code

--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,9 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'rewardStyleServices', 'rsDirectives']);
+angular.module('myApp', ['myApp.filters', 'rewardStyleServices', 'rsDirectives'])
+ .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.
+          when('/', {templateUrl: 'partials/featured.html', controller: 'FeaturedCtrl'}).
+      otherwise({redirectTo: '/phones'});
+  }]);
